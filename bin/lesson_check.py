f7892ec68e9eec56b134786f4aee0f544f5f14e4
Kilian Lieret <kilian.lieret@posteo.de>
2022-08-11 14:38:24 -0400

    Fix merge conflict

--- bin/lesson_check.py
+++ bin/lesson_check.py
@@ -221,11 +221,7 @@ def check_config(reporter, source_dir):
     reporter.check_field(config_file, 'configuration',
                          config, 'kind', 'lesson')
     reporter.check_field(config_file, 'configuration',
-<<<<<<< HEAD
-                         config, 'carpentry', ('hsf', 'swc', 'dc', 'lc', 'cp'))
-=======
                          config, 'carpentry', ('swc', 'dc', 'lc', 'cp', 'incubator'))
->>>>>>> 14d73565c21f41fb51ca70cf7eefe8cfa73529d2
     reporter.check_field(config_file, 'configuration', config, 'title')
     reporter.check_field(config_file, 'configuration', config, 'email')
 
